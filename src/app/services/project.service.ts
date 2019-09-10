74e156a0a9a9e5dc2fb0ee50095980961f6facc8
unknown <d.munozmauro@hotmail.com>
2019-09-10 18:33:17 -0300

    Proyecto para uso de curso master angular front y back

---
+++ src/app/services/project.service.ts
@@ -0,0 +1,19 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { Project } from '../models/project';
+import { global } from './global';
+@Injectable ()
+export class ProjectService {
+    public url: string;
+
+    constructor (
+        private _http: HttpClient
+    ) {
+        this.url = global.url;
+    }
+
+    testServices() {
+        return 'probando servicio angular';
+    }
+}
